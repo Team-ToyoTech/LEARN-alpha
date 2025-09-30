e501c50da760de4ce2b945dadedfda50dc446826
DH.L <107869892+meozigoon@users.noreply.github.com>
2025-09-30 18:55:12 +0900

    Add interactive port connections on main form

--- LEARN-alpha/Learn.Designer.cs
+++ LEARN-alpha/Learn.Designer.cs
@@ -1,11 +1,14 @@
-﻿namespace LEARN_alpha
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LEARN_alpha
 {
     partial class Learn
     {
         /// <summary>
         ///  Required designer variable.
         /// </summary>
-        private System.ComponentModel.IContainer components = null;
+        private System.ComponentModel.IContainer? components = null;
 
         /// <summary>
         ///  Clean up any resources being used.
@@ -28,18 +31,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            connectionCanvas = new Panel();
             SuspendLayout();
-            // 
+            //
+            // connectionCanvas
+            //
+            connectionCanvas.BackColor = Color.WhiteSmoke;
+            connectionCanvas.BorderStyle = BorderStyle.FixedSingle;
+            connectionCanvas.Cursor = Cursors.Cross;
+            connectionCanvas.Dock = DockStyle.Fill;
+            connectionCanvas.Location = new Point(0, 0);
+            connectionCanvas.Name = "connectionCanvas";
+            connectionCanvas.Size = new Size(800, 450);
+            connectionCanvas.TabIndex = 0;
+            //
             // Learn
-            // 
+            //
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(connectionCanvas);
             Name = "Learn";
+            Padding = new Padding(12);
             Text = "LEARN ver.α";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel connectionCanvas;
     }
 }
--- LEARN-alpha/Logic.cs
+++ LEARN-alpha/Logic.cs
@@ -1,57 +1,91 @@
-﻿namespace LEARN_alpha
+using System;
+using System.Collections.Generic;
+
+namespace LEARN_alpha
 {
     internal class Logic
     {
-        List<Logic> in1 = [];
-        List<Logic> in2 = [];
-        List<Logic> out1 = [];
-        List<Logic> out2 = [];
+        private readonly Dictionary<ConnectionType, List<Logic>> _connections;
 
-        int id = 0;
+        private readonly int id;
 
         public Logic(int id)
         {
             this.id = id;
+            _connections = new Dictionary<ConnectionType, List<Logic>>();
+
+            foreach (ConnectionType type in Enum.GetValues(typeof(ConnectionType)))
+            {
+                _connections[type] = new List<Logic>();
+            }
         }
 
-        public static bool operator==(Logic a, Logic b)
+        public int Id => id;
+
+        public static bool operator ==(Logic? a, Logic? b)
         {
+            if (ReferenceEquals(a, b))
+            {
+                return true;
+            }
+
+            if (a is null || b is null)
+            {
+                return false;
+            }
+
             return a.id == b.id;
         }
 
-        public static bool operator!=(Logic a, Logic b)
+        public static bool operator !=(Logic? a, Logic? b)
         {
-            return a.id != b.id;
+            return !(a == b);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Logic other && other.id == id;
+        }
+
+        public override int GetHashCode()
+        {
+            return id.GetHashCode();
         }
 
         public enum ConnectionType
         {
             Input1,
             Input2,
+            Input3,
+            Input4,
             Output1,
-            Output2
+            Output2,
+            Output3,
+            Output4
+        }
+
+        public IReadOnlyList<Logic> GetConnections(ConnectionType connectionType)
+        {
+            if (!_connections.TryGetValue(connectionType, out var connections))
+            {
+                throw new ArgumentOutOfRangeException(nameof(connectionType), connectionType, null);
+            }
+
+            return connections;
         }
 
         public void Connect(Logic other, ConnectionType connectionType)
         {
             ArgumentNullException.ThrowIfNull(other);
 
-            switch (connectionType)
+            if (!_connections.TryGetValue(connectionType, out var connections))
             {
-                case ConnectionType.Input1:
-                    in1.Add(other);
-                    break;
-                case ConnectionType.Input2:
-                    in2.Add(other);
-                    break;
-                case ConnectionType.Output1:
-                    out1.Add(other);
-                    break;
-                case ConnectionType.Output2:
-                    out2.Add(other);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(connectionType), connectionType, null);
+                throw new ArgumentOutOfRangeException(nameof(connectionType), connectionType, null);
+            }
+
+            if (!connections.Contains(other))
+            {
+                connections.Add(other);
             }
         }
     }
