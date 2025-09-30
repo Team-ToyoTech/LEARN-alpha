713f3476dcd0d99eaf574658747fbb1ee00051ed
endiy0 <steve060401@gmail.com>
2025-10-01 08:54:39 +0900

    logic connection added

--- LEARN-alpha/Logic.cs
+++ LEARN-alpha/Logic.cs
@@ -2,16 +2,23 @@
 {
     internal class Logic
     {
-        List<Logic> in1 = [];
-        List<Logic> in2 = [];
-        List<Logic> out1 = [];
-        List<Logic> out2 = [];
+        List<(Logic logic, ConnectionType type)> in1 = [];
+        List<(Logic logic, ConnectionType type)> in2 = [];
+        List<(Logic logic, ConnectionType type)> out1 = [];
+        List<(Logic logic, ConnectionType type)> out2 = [];
 
+        bool in1State = false;
+        bool in2State = false;
+        bool out1State = false;
+        bool out2State = false;
+
+        LogicType type;
         int id = 0;
 
-        public Logic(int id)
+        public Logic(int id, LogicType Type)
         {
             this.id = id;
+            type = Type;
         }
 
         public static bool operator==(Logic a, Logic b)
@@ -24,6 +31,17 @@
             return a.id != b.id;
         }
 
+        public enum LogicType
+        {
+            AND,
+            OR,
+            NOT,
+            NAND,
+            NOR,
+            XOR,
+            XNOR
+        }
+
         public enum ConnectionType
         {
             Input1,
@@ -32,23 +50,23 @@
             Output2
         }
 
-        public void Connect(Logic other, ConnectionType connectionType)
+        public void Connect(Logic other, ConnectionType connectionType, ConnectionType myconnection)
         {
             ArgumentNullException.ThrowIfNull(other);
 
             switch (connectionType)
             {
                 case ConnectionType.Input1:
-                    in1.Add(other);
+                    in1.Add((other, myconnection));
                     break;
                 case ConnectionType.Input2:
-                    in2.Add(other);
+                    in2.Add((other, myconnection));
                     break;
                 case ConnectionType.Output1:
-                    out1.Add(other);
+                    out1.Add((other, myconnection));
                     break;
                 case ConnectionType.Output2:
-                    out2.Add(other);
+                    out2.Add((other, myconnection));
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(connectionType), connectionType, null);
